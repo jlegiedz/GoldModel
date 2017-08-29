b93134b241dedc96020efde810419fcfe40a88a6
Joanna Legiedz <joanna.legiedz@gmail.com>
2017-08-29 21:00:43 +0200

    CSV writer added

--- Gold/src/main/java/Constants.java
+++ Gold/src/main/java/Constants.java
@@ -2,4 +2,7 @@
 public class Constants {
     public static final String GOLD_BASE = "http://api.nbp.pl/api/";
     public static final String GOLD_CENA = "cenyzlota/last";
+
+    public static final Object [] FILE_HEADER = {"Price","Average Price","Recommendation","Clause"};
+    public static final String separator = " ,";
 }
--- Gold/src/main/java/Main.java
+++ Gold/src/main/java/Main.java
@@ -1,7 +1,12 @@
-import retrofit2.converter.gson.GsonConverterFactory;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.io.IOException;
+import javax.swing.text.html.parser.Parser;
+import java.io.*;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,32 +21,59 @@ public class Main {
 
         NBP api = retrofit.create(NBP.class);
 
-        GoldQuery query =  new GoldQuery();
+        GoldQuery query = new GoldQuery();
 
-        List<GoldModel> responseList = query.execute(api,3);
-        if(!responseList.isEmpty()){
+        List<GoldModel> responseList = query.execute(api, 3);
+        if (!responseList.isEmpty()) {
             printGold(responseList);
         }
+        System.out.print("Srednia cena zlota: ");
         System.out.println(average(responseList));
 
+        File csv = Paths.get("C:\\Exercism\\java\\gold\\", "nazwa.csv").toFile();
+//        Reader reader = new FileReader(csv);
+
+
+
+        writeCSVFile(responseList, "nazwa.csv");
 
     }
 
-    public static void printGold(List<GoldModel> list){
+    public static void printGold(List<GoldModel> list) {
         list.forEach(goldModel -> System.out.println(goldModel.getCena()));
     }
-    public static double average(List<GoldModel> list){
+
+    public static double average(List<GoldModel> list) {
         double average = 0;
         double suma = 0;
         double num = list.size();
         List<Double> priceList = new ArrayList<>();
-        list.forEach(goldModel ->  priceList.add(goldModel.getCena()));
-        for (int i =0; i < priceList.size(); i++) {
-        suma = suma + priceList.get(i);
+        list.forEach(goldModel -> priceList.add(goldModel.getCena()));
+        for (int i = 0; i < priceList.size(); i++) {
+            suma = suma + priceList.get(i);
         }
-        average = suma/priceList.size();
-        System.out.print("Srednia cena zlota: ");
+        average = suma / priceList.size();
         return average;
+    }
+
+    public static void writeCSVFile(List<GoldModel> responseList, String fileName) throws IOException {
+
+        String csv = "C:\\Exercism\\java\\gold";
+        FileWriter fileWriter = null;
+
+        fileWriter = new FileWriter(fileName);
+        CSVPrinter csvFilePrinter = CSVFormat.RFC4180.print(fileWriter);
+
+        List<Double> priceList = new ArrayList<>();
+        csvFilePrinter.printRecord(Constants.FILE_HEADER);
+        responseList.forEach(goldModel -> priceList.add(goldModel.getCena()));
+        csvFilePrinter.printRecord(priceList.get(0), average(responseList));
+        csvFilePrinter.printRecords(priceList.subList(1,priceList.size()));
+
+        csvFilePrinter.close();
+    }
+    public static void readCSVFile(Reader reader){
+
     }
 
 }
