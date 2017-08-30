ca00e9590d5127bd38d92f55d7eecef225aba9b6
Joanna Legiedz <joanna.legiedz@gmail.com>
2017-08-30 20:56:19 +0200

    next points of exercise

--- Gold/src/main/java/Main.java
+++ Gold/src/main/java/Main.java
@@ -1,10 +1,10 @@
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import javax.swing.text.html.parser.Parser;
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -23,19 +23,33 @@ public class Main {
 
         GoldQuery query = new GoldQuery();
 
-        List<GoldModel> responseList = query.execute(api, 3);
+        List<GoldModel> responseList = query.execute(api, 8);
         if (!responseList.isEmpty()) {
             printGold(responseList);
         }
         System.out.print("Srednia cena zlota: ");
         System.out.println(average(responseList));
 
-        File csv = Paths.get("C:\\Exercism\\java\\gold\\", "nazwa.csv").toFile();
-//        Reader reader = new FileReader(csv);
-
-
-
         writeCSVFile(responseList, "nazwa.csv");
+
+        double averageOfthreeDays =averageOfLastThreeDays(responseList);
+        System.out.println("Srednia z 3 dni " + averageOfthreeDays);
+        if(averageOfthreeDays) {
+
+            double percentage = (diff/average(responseList))*100;
+            System.out.println(percentage);
+             diff = average(responseThreeDays)- average(responseList);
+             percentage = (diff/average(responseList))*100;
+            System.out.println(percentage);
+
+
+            if (averageOfthreeDays > average(responseList)) {
+                System.out.println("Sprzedawaj");
+            }
+            if (averageOfthreeDays < average(responseList)) {
+                System.out.println("Kupuj");
+            }
+        }
 
     }
 
@@ -68,12 +82,34 @@ public class Main {
         csvFilePrinter.printRecord(Constants.FILE_HEADER);
         responseList.forEach(goldModel -> priceList.add(goldModel.getCena()));
         csvFilePrinter.printRecord(priceList.get(0), average(responseList));
-        csvFilePrinter.printRecords(priceList.subList(1,priceList.size()));
+        csvFilePrinter.printRecords(priceList.subList(1, priceList.size()));
 
         csvFilePrinter.close();
-    }
-    public static void readCSVFile(Reader reader){
 
     }
+
+
+    public static double averageOfLastThreeDays(List<GoldModel> responseThreeDays){
+        return (responseThreeDays.get(0).getCena() +responseThreeDays.get(1).getCena()+ responseThreeDays.get(2).getCena())/3;
+    }
+
+//    public static List readCSV() throws FileNotFoundException, IOException {
+//        List prices = new ArrayList<>();
+//
+//        BufferedReader br = new BufferedReader(new FileReader("nazwa.csv"));
+//
+//        String line = br.readLine();
+//        while ((line = br.readLine()) != null && !line.isEmpty()) {
+//            String[] lineSplitted = line.split(",");
+//            for (int i = 0; i <lineSplitted.length ; i++) {
+//                String firstPrice = lineSplitted[i];
+//                prices.add(firstPrice);
+//            }
+//        }
+//        br.close();
+//
+//        return prices;
+//    }
+
 
 }
